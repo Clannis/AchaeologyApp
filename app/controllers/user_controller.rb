33bdb899a64339bf2273740c4b9dd327477c7b5d
Clannis <Dodds1990@gmail.com>
2020-06-08 08:45:08 -0500

    add user login get and post routes

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -20,4 +20,22 @@ class UserController < ApplicationController
         redirect "/signup"
     end
     
+    get "/login" do
+        if session[:user_id]
+          redirect "/dig_sites/index"
+        else
+          erb :login
+        end
+    end
+
+    post "/login" do 
+        user = User.find_by(username: params[:username])
+        if user && user.authenticate(params[:password])
+          session[:user_id] = user.id
+          redirect "/dig_sites/index"
+        else
+          redirect "/login"
+        end
+    end
+
 end
