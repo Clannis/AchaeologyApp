6f58d7e7b8eee1aab76bc7463403362226265de9
Clannis <dodds1990@gmail.com>
2020-06-10 22:44:55 -0500

    move helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,5 +12,21 @@ class ApplicationController < Sinatra::Base
     get "/" do
         erb :index
     end
+
+    helpers do
+
+        def current_user
+            User.find(session[:user_id])
+        end
+    
+        def logged_in?
+            !!session[:user_id]
+        end
+    
+        def authenticate
+            redirect "/login" if !logged_in?
+        end
+
+    end
     
 end
