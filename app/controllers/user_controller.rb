1042acd82ded6bdc4a5b564ae9e65e27e13c778d
Clannis <dodds1990@gmail.com>
2020-06-11 03:49:33 -0500

    added user update routes and forms

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -60,9 +60,31 @@ class UserController < ApplicationController
         end
     end
 
-    get "/user" do
+    get "/users/:id" do
       @user = current_user
+      erb :"/users/show"
+    end
+
+    get "/users/:id/edit" do
+      @user = User.find(params[:id])
       erb :"/users/edit"
+    end
+
+    patch "/users/:id" do
+      @user = User.find(params[:id])
+      if @user == current_user
+        if !params[:name].empty?
+          @user.name = params[:name]
+        end
+        if !params[:email].empty?
+          @user.email = params[:email]
+        end
+        @user.save
+        redirect "/user/#{current_user.id}"
+      else
+        error
+        redirect "/user/#{current_user.id}"
+      end
     end
     
 end
