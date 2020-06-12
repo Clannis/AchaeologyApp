35de5fbf175b0adbea6812f134539d6f0182ae51
Clannis <dodds1990@gmail.com>
2020-06-11 21:34:57 -0500

    added CRUD functionallity to artifacts

--- app/controllers/artifact_controller.rb
+++ app/controllers/artifact_controller.rb
@@ -1,4 +1,9 @@
 class ArtifactController < ApplicationController
+
+    get "/levels/:id/artifacts/new" do
+        @level = Level.find(params[:id])
+        erb :"/artifacts/new"
+    end
     
     get "/artifacts/:id" do
         @artifact = Artifact.find(params[:id])
@@ -9,4 +14,26 @@ class ArtifactController < ApplicationController
         end
         erb :"/artifacts/show"
     end
+
+    post "/levels/:id/artifacts/new" do
+        @level = Level.find(params[:id])
+        @artifact = Artifact.new(local_id: params[:local_id], artifact_type: params[:artifact_type], description: params[:description], level_id: @level.id)
+        exists = false
+        @level.artifacts.each do |artifact|
+            if artifact.local_id == @artifact.local_id
+                exists = true
+            end
+        end
+
+        if !exists
+            if @artifact.save
+                redirect "/artifacts/#{@artifact.id}"
+            else
+                redirect "/levels/#{@level.id}/artifact/new"
+            end
+        else
+            redirect "/levels/#{@level.id}/artifact/new"
+        end
+    end
+
 end
