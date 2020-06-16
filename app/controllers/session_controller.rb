4c44166b354dc6f327ca1379f720e9114d3843f4
Clannis <dodds1990@gmail.com>
2020-06-16 00:34:48 -0500

    relocate sessions manipulation to new session controller

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,67 @@
+class SessionController < ApplicationController
+
+    get "/signup" do
+        logged_in_redirect
+        if !session[:user_id]
+          @error = false
+          erb :"/users/create_user"
+        else
+          redirect "/dig_sites"
+        end
+    end
+
+    post "/signup" do
+        logged_in_redirect
+        if !params[:username].empty? && (!params[:email].empty? && params[:email].include?("@")) && !params[:password].empty? && !params[:name].empty?
+            @user = User.new(username: params[:username], email: params[:email], password: params[:password], name: slug(params[:name]))
+            if @user.save
+              session[:user_id] = @user.id
+              redirect "/dig_sites"
+            else
+              erb :"/users/create_user"
+            end
+        end
+        @error = "All fields must be filled in."
+        erb :"/users/create_user"
+    end
+    
+    get "/login" do
+        logged_in_redirect
+        if session[:user_id]
+          redirect "/dig_sites"
+        else
+          erb :"/users/login"
+        end
+    end
+
+    post "/login" do 
+        logged_in_redirect
+        if params[:user].include?("@")
+            user = User.find_by(email: params[:user])
+        else
+            user = User.find_by(username: params[:user])
+        end
+        if user 
+          if user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect "/dig_sites"
+          else
+            @error = "Incorrect password"
+            erb :"/users/login"
+          end
+        else
+          @error = "Incorrect Username/Email"
+          erb :"/users/login"
+        end
+    end
+
+    get "/logout" do
+        if session[:user_id]
+            session.clear
+            redirect "/login"
+        else
+            redirect "/"
+        end
+    end
+
+end
--- config.ru
+++ config.ru
@@ -1,6 +1,7 @@
 require './config/environment'
 
 use Rack::MethodOverride
+use SessionController
 use ArtifactController
 use DigSiteController
 use LevelController
