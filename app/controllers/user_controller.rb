e37f969a38af59b08f8cf08a87e6899822f39ce3
Clannis <dodds1990@gmail.com>
2020-06-16 11:57:52 -0500

    add validation to user updates

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,16 +1,19 @@
 class UserController < ApplicationController
 
     get "/users/:id" do
+      authenticate
       @user = current_user
       erb :"/users/show"
     end
 
     get "/users/:id/edit" do
+      authenticate
       @user = User.find(params[:id])
       erb :"/users/edit"
     end
 
     patch "/users/:id" do
+      authenticate
       @user = User.find(params[:id])
       if @user == current_user
         if !params[:name].empty?
@@ -19,14 +22,21 @@ class UserController < ApplicationController
         if !params[:email].empty?
           @user.email = params[:email]
         end
-        if !params[:password].empty?
-          @user.password = params[:password]
+        if !params[:new_password].empty?
+          @user.update(password: params[:new_password], password_confirmation: params[:new_password_confirmation])
         end
-        @user.save
-        redirect "/users/#{current_user.id}"
+        if @user && @user.authenticate(params[:password])
+          @user.save
+          if @user.errors.any?
+            erb :"/users/edit"
+          else
+            redirect "/users/#{current_user.id}"
+          end
+        else
+          erb :"/users/edit"
+        end 
       else
-        error
-        redirect "/users/#{current_user.id}"
+        erb :"/users/edit"
       end
     end
     
