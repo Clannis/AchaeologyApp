8cc0e4983561b24f64464092863cb9622934ee43
Clannis <dodds1990@gmail.com>
2020-06-16 04:58:05 -0500

    add css for navbars and background

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@
 class ApplicationController < Sinatra::Base
 
     configure do
-        set :public_folder, 'public'
+        set :public_folder, Proc.new { File.join(root, "public") }
         set :views , 'app/views'
         enable :sessions
         set :session_secret, ENV['SESSION_SECRET']
